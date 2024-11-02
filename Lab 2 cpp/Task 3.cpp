368f1339819366db1f863524ec22bc36e0fa7ef9
IzzaMahmood <63879@students.riphah.edu.pk>
2024-11-02 16:43:41 +0500

    Add files via upload

---
+++ Lab 2 cpp/Task 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+    string name;
+    int sapId;
+    float cgpa;
+    cout << "Enter your Name: ";
+    cin>>name;
+    cout << "Enter your SAP ID: ";
+    cin >> sapId;
+    cout << "Enter your CGPA: ";
+    cin >> cgpa;
+    return 0;
+}
+
